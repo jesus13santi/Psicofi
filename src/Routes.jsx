33e83db27a6abc274f4df7d68ba730fe483ab646
Jesus <jesus13santi@gmail.com>
2021-11-14 15:46:02 -0400

    Conflicto resuelto definitivo

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import PsicologosPage from "./pages/PsicologosPage";
 import PreciosPage from "./pages/PreciosPage";
 import TableroPage from './pages/TableroPage';
+import PerfilPaciente from './components/PerfilPaciente/PerfilPaciente'
+import PerfilEspecialista from './components/PerfilEspecialista/PerfilEspecialista'
 function Routes() {
   return (
     <Switch>
@@ -14,6 +15,8 @@ function Routes() {
       <Route exact path="/register" component={RegisterPage}></Route>
       <Route exact path="/login" component={LoginPage}></Route>
       <Route exact path="/precio" component={PreciosPage}></Route>
+      <Route exact path="/perfilPaciente" component={PerfilPaciente}></Route>
+        <Route exact path="/perfilEspecialista" component={PerfilEspecialista}></Route>
       <Route exact path="/deck" component={TableroPage}></Route>
       <Route exact path="/" component={HomePage}></Route>
       <Route exact path="*">
@@ -21,27 +24,6 @@ function Routes() {
       </Route>
     </Switch>
   );
-=======
-import HomePage from "./pages/HomePage"
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import PerfilPaciente from './components/PerfilPaciente/PerfilPaciente'
-import PerfilEspecialista from './components/PerfilEspecialista/PerfilEspecialista'
-
-function Routes(){
-    return (
-      <Switch>
-        <Route exact path="/register" component={RegisterPage}></Route>
-        <Route exact path="/login" component={LoginPage}></Route>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/perfilPaciente" component={PerfilPaciente}></Route>
-        <Route exact path="/perfilEspecialista" component={PerfilEspecialista}></Route>
-        <Route exact path="*">
-          <h1>"404: Page not found"</h1>
-        </Route>
-      </Switch>
-    );
->>>>>>> develop/Fabiola
 }
 
 export default Routes;
