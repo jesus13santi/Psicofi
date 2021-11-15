102eaf96792c302cbe237cee69fd45d318dd2088
Jesus <jesus13santi@gmail.com>
2021-11-14 20:25:01 -0400

    Login listo css, falta funcionalidad

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,29 +1,189 @@
-import { auth, googleProvider } from '../../utils/firebaseConfig';
-import { useHistory } from 'react-router-dom';
+import { useState, useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { auth, googleProvider } from "../../utils/firebaseConfig";
+import { Link, useHistory } from "react-router-dom";
+import styles from "./LoginForm.module.css";
+import { validateEmail } from "../../utils/helpers.js";
+import { size } from "lodash";
 
+function LoginForm() {
+  const history = useHistory();
+  const { createUser } = useContext(UserContext);
 
-function LoginForm () {
+  const [errorName, setErrorName] = useState("");
+  const [errorLastName, setErrorLastName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorPasswordB, setErrorPasswordB] = useState("");
+  const [errorNumber, setErrorNumber] = useState("");
 
-    const history = useHistory();
+  const [role, setRole] = useState("");
+  const cambioRole = (a) => {
+    setRole(a.target.value);
+  };
 
-    // GOOGLE AUTH
-    const handleGoogleLogin = async () => {
-        console.log('Google Login Init');
-        const response = await auth.signInWithPopup(googleProvider);
-        console.log({response: response.user});
-        history.push('/');
-    };
+  const [values, setValues] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+    passwordB: "",
+    number: "",
+  });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('Login Password');
-    };
+  const handleGoogleLogin = async () => {
+    await auth.signInWithPopup(googleProvider);
+  };
 
-    return (
-    <div>
-        <h1>
-            "Login with Google test"
-        </h1>
-    </div>
-    )
+  const handleOnChange = (event) => {
+    const { value, name: inputName } = event.target;
+    console.log({ inputName, value });
+    setValues({ ...values, [inputName]: value });
+  };
+
+  const validateData = () => {
+    setErrorName("");
+    setErrorLastName("");
+    setErrorPasswordB("");
+    setErrorEmail("");
+    setErrorPassword("");
+    setErrorNumber("");
+    let isValid = true;
+
+    if (size(values.name) < 3) {
+      setErrorName("Debes ingresar un nombre de al menos tres carácteres.");
+      isValid = false;
+    }
+
+    if (size(values.lastname) < 3) {
+      setErrorLastName(
+        "Debes ingresar una contraseña de al menos tres carácteres."
+      );
+      isValid = false;
+    }
+
+    if (!validateEmail(values.email)) {
+      setErrorEmail("Debes de ingresar un email válido.");
+      isValid = false;
+    }
+
+    if (size(values.password) < 8) {
+      setErrorPassword(
+        "Debes ingresar una contraseña de al menos ocho carácteres."
+      );
+      isValid = false;
+    }
+
+    if (size(values.passwordB) < 8) {
+      setErrorPasswordB(
+        "Debes ingresar una confirmación de contraseña de al menos ocho carácteres."
+      );
+      isValid = false;
+    }
+
+    if (values.password !== values.passwordB) {
+      setErrorPassword("Asegure que la contraseñas coincidan.");
+      setErrorPasswordB("Asegure que la contraseñas coincidan.");
+      isValid = false;
+      console.log("no ta iguales");
+    }
+
+    if (size(values.number) < 1) {
+      setErrorName("Ingrese un numero de telefono valido");
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await auth.signInWithEmailAndPassword(values.email, values.password);
+    history.push("/");
+    // console.log("LOGIN_PASSWOROD");
+  };
+
+  return (
+    <section className={styles.registerSecc}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h2 className={styles.tittle}> Que bueno tenerte </h2>
+          <p className={styles.nextText}> Continua con... </p>
+          <div className={styles.socialButtons}>
+            <button
+              className={styles.img1}
+              type="button"
+              onClick={handleGoogleLogin}
+            >
+              {" "}
+            </button>
+
+            <button
+              className={styles.img2}
+              type="button"
+              onClick={handleGoogleLogin}
+            >
+              {" "}
+            </button>
+
+            <button
+              className={styles.img3}
+              type="button"
+              onClick={handleGoogleLogin}
+            >
+              {" "}
+            </button>
+          </div>
+          <p className={styles.nextText}> o correo electronico: </p>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+
+          <div className={styles.input2}>
+            <input
+              className={styles.formRegis}
+              name="email"
+              id={styles.email}
+              type="email"
+              placeholder="Enter your email"
+              value={values.email}
+              onChange={handleOnChange}
+              errorMessage={errorEmail}
+            />
+
+            <input
+              className={styles.formRegis}
+              name="password"
+              id={styles.password}
+              type="password"
+              placeholder="Enter your password"
+              value={values.password}
+              onChange={handleOnChange}
+              errorMessage={errorPassword}
+            />
+
+            
+          </div>
+
+          
+
+          <div className={styles.submit}>
+            <button
+              className={styles.continue}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              {" "}
+              Continuar{" "}
+            </button>
+            <p id={styles.loginNext} className={styles.nextText}>
+              {" "}
+              ¿No tienes una cuenta? <Link to="/register">Registrate</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
+export default LoginForm;
