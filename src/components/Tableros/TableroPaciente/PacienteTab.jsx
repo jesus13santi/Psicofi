f929835600af683e5d1b46470bef85151008fc6a
Jesus <jesus13santi@gmail.com>
2021-11-15 15:22:31 -0400

    Resuelto el problema de tab paciente

--- src/components/Tableros/TableroPaciente/PacienteTab.jsx
+++ src/components/Tableros/TableroPaciente/PacienteTab.jsx
@@ -4,12 +4,19 @@ import UTabCard from "../../UTabCard/UTabCard";
 import CardStory from "../../CardStory/CardStory";
 import { Link } from "react-router-dom";
 import Footer from "../../Footer/Footer";
-
+import { useContext } from "react";
+import { UserContext } from "../../../context/UserContext";
 
 const PacienteTab =  () => {
+    const {user, setUser}= useContext(UserContext)
+    
+
+
+
+    console.log(user)
     return (
     <div className={styles.container}>
-    <h1 className={styles.title}>Bienvenido, NOMBRE</h1>
+    <h1 className={styles.title}>Bienvenido, {!!user?(user.name): (console.log("cargando"))} </h1>
     <div className={styles.box}>
         <h1 className={styles.boxTitle}>Citas próximas:</h1>
         <div className={styles.citasProximas}>
