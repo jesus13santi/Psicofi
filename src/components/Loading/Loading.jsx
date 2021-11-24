3a9ff7590abb067099408f17ab655aad778ad3e0
cbehrensunimet <c.behrens@correo.unimet.edu.ve>
2021-11-23 23:32:16 -0400

    Update Loading.jsx

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import PulseLoader from "react-spinners/PulseLoader";
+import PulseLoader from "react-spinners/PulseLoader";
 import styles from "./Loading.module.css"
 
 const Loading = () => {
     return (
         <div className= {styles.loading}>
             <div className= {styles.text}>Cargando</div>
-            {/* <PulseLoader color={"#763D80"} loading={true} size={25} css={styles.loading} /> */}
+            <PulseLoader color={"#763D80"} loading={true} size={25} css={styles.loading} />
         </div>
     )
 }
