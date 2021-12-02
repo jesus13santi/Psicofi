e436c7973950df72620b5e7b8622127ed42f389b
cbehrensunimet <c.behrens@correo.unimet.edu.ve>
2021-12-01 21:27:43 -0400

    Update PerfilEspecialista.jsx

--- src/components/PerfilEspecialista/PerfilEspecialista.jsx
+++ src/components/PerfilEspecialista/PerfilEspecialista.jsx
@@ -6,7 +6,6 @@ import styles from "./PerfilEspecialista.module.css";
 import { db } from "../../utils/firebaseConfig";
 import $ from "jquery";
 import firebase from "firebase";
-import Footer from "../Footer/Footer";
 //import es from "date-fns/locale/es";
 import uniqid from "uniqid";
 import Loading from "../Loading/Loading";
@@ -197,6 +196,11 @@ const PerfilEspecialista = () => {
       } else {
         setError("Ya ingreso una cita con esa fecha y hora");
       }
+      
+    
+    
+    
+    
     
   };
   const deleteAppointment = async (id) => {
@@ -209,7 +213,6 @@ const PerfilEspecialista = () => {
     setUser(updateUser);
     alert("Cita Eliminada");
   };
-// Funcion para mostrar la fecha de la cita
   function diaSemana(x) {
     const date1 = new Date(x.replace(/-+/g, "/"));
     const options = {
@@ -222,7 +225,6 @@ const PerfilEspecialista = () => {
     // console.log(date1.toLocaleDateString("es-MX", options));
     return result;
   }
-  // Funcion para mostrar la fecha de su cumpleaños
   function diaBirthday(x) {
     const date1 = new Date(x.replace(/-+/g, "/"));
     const options = {
@@ -256,7 +258,6 @@ const PerfilEspecialista = () => {
   }
 
   return (
-    <>
     <div className={`${styles.fondorosa} ${styles.bordecontenedor}`}>
       <div className={styles.titulo}>Mi perfil</div>
       <form>
@@ -478,8 +479,7 @@ const PerfilEspecialista = () => {
 
                 {ordenar(user.appointments).map((m) => (
                   <>
-                    {console.log(user.appointments.date)}
-                    {m.status === 2 && date < new Date(m.date)  && (
+                    {m.status === 2 && (
                       <div key={m.id} className={styles.cita}>
                         <p className={styles.grid}>{diaSemana(m.date)}</p>
                         <p className={styles.grid}>
@@ -503,8 +503,6 @@ const PerfilEspecialista = () => {
         </div>
       </div>
     </div>
-    <Footer/>
-    </>
   );
 };
 
