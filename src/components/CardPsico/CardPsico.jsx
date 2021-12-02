2cf904351eaa258a67afd5a831bf4ec2118c0fef
cbehrensunimet <c.behrens@correo.unimet.edu.ve>
2021-12-02 05:20:55 -0400

    Merge Final (?)

--- src/components/CardPsico/CardPsico.jsx
+++ src/components/CardPsico/CardPsico.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./CardPsico.module.css";
 import { Link, useHistory } from "react-router-dom";
+import start from "../../img/start.png"
 
-const CardPsico = ({ id, name, pais, photo, description,problemas }) => {
+
+const CardPsico = ({ id, name, pais, photo, description, problemas, rating }) => {
   const history = useHistory();
+  let total = rating.reduce((a, b) => a + b, 0);
   const handlePatientHistory = () => {
     history.push(`/profile/${id}`);
   };
@@ -19,14 +22,24 @@ const CardPsico = ({ id, name, pais, photo, description,problemas }) => {
               <h2 className={styles.title}>{name}</h2>
             </Link>
             <div className={styles.pais}>
-              <p>País:</p>
+              <p className = {styles.ptitle}>País:</p>
               <p>{pais}</p>
             </div>
+            <div className={styles.ratingData}>
+            <div className ={styles.ratingData}>
+              <p className = {styles.ptitle}>Valoración:</p>
+              <p>{(total / rating.length).toFixed(1)}</p>
+              <p>★</p>
+            </div>
+            </div>
             {problemas.length>0 && (
-              <p>Especialidades: {problemas.toString().replace(/,/g, ", ")}</p>
+              <>
+              <p className = {styles.ptitle}>Especialidades:</p>
+              <p > {problemas.toString().replace(/,/g, ", ")}</p>
+              </>
             )}
 
-            <p className={styles.description}>{description}</p>
+            {/* <p className={styles.description}>{description}</p> */}
           </div>
         </div>
         <button
